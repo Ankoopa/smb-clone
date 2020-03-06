af67b237e3b91144ec0867545ea57cafaa0b45c4
Ankoopa <dmdasalla@mcm.edu.ph>
2020-03-07 03:07:42 +0800

    design files

---
+++ mario/smb-clone/js/game.js
@@ -0,0 +1,15 @@
+var config = {
+    type: Phaser.AUTO,
+      width: 1280,
+      height: 720,
+      physics: {
+          default: 'arcade',
+          arcade: {
+              gravity: {y: 575},
+              debug: true
+          }
+      },
+    scene: [Menu, Level1, Level2, Death]
+  }
+  
+var game = new Phaser.Game(config);
